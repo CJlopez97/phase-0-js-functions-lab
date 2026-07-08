bdeb5e7532b54a9c564b3a8251f2be7e92165911
Cristian J Lopez-Ramos <crisjossua@gmail.com>
2026-07-07 23:29:42 -0600

    Functions have been created and tested

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+function calculateTax(amount){
+    let tax = amount * .1;
+    return tax;
+}
+function convertToUpperCase(text){
+    return text.toUpperCase();
+}
+function findMaximum(num1, num2){
+    if(num1 > num2){
+        return num1;
+    } else {
+        return num2;
+    }
+}
+function isPalindrome(word){
+     let palindrome = word.split("").reverse().join("");
 
-
-
-
-
+     return word === palindrome;
+}
+function calculateDiscountedPrice(originalPrice, discountPercentage){
+    let discount = originalPrice * discountPercentage/100
+    let newPrice = originalPrice - discount
+    return newPrice 
+}
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
